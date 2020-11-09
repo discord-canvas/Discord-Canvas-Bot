a5085b95b8bf83496d0758a26037ba570442a9f6
Cammy Fentreux <cameron1709@live.co.uk>
2020-11-09 20:03:17 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv').config()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
