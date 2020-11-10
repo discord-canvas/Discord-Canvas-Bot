77c6b24e7fe8ec5172e48c3911cd5033cfca5f3a
Cammy Fentreux <cameron1709@live.co.uk>
2020-11-10 11:23:13 +0000

    hardcoded reply for example question using new tree system

--- index.js
+++ index.js
@@ -3,34 +3,34 @@ const client = new Discord.Client();
 const fs = require('fs')
 require('dotenv').config()
 
-// image  = fs.readFileSync("questions/105.1.install haskell.png", () => console.log("Image loaded"))
+const homework = JSON.parse(fs.readFileSync('homework.json'))
+console.log(homework['105'])
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  prefix = "!"
-  if (!msg.content.startsWith(prefix) || msg.author.bot) return
+  prefix = '!'
+  if (!msg.content.startsWith(prefix) || msg.author.bot) {return}
 
   const args = msg.content.slice(prefix.length).trim().split(' ')
   const command = args.shift().toLowerCase()
-  console.log(args, command);
+
   if (command === 'homework') {
-    id = args.join('.')
-    console.log(id)
-    image = fs.readFileSync("questions/" + id + ".png")
-    msg.channel.send('Pong!', {files: [image]});
+    qNode = homework['105'].children['1'].children['pure-functions'].children['1']
+    image = fs.readFileSync(qNode.image)
+    msg.channel.send('', {files:[image]})
   }
+})
 
-  // if (msg.content.toLowerCase() === 'ping') {
-  //   id = "105.1.install haskell"
-  //   image = fs.readFileSync("questions/" + id + ".png")
-  //   msg.channel.send('Pong!', {files: [image]});
-  // }
-});
 
-// saveHomework (homework) {}
+function sendNode(destination, node) {
+  if (node.image) {
+    image = fs.readFileSync(node.image)
+    destination.send(node.children, {files:image})
+  }
+}
 
 client.login(process.env.BOT_TOKEN);
 
