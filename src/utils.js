23fde90269409426b1d3e03c3e04c600c14fb849
Douile <25043847+Douile@users.noreply.github.com>
2020-11-11 19:23:34 +0000

    Add documentation for utils functions

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,18 @@
 const MS_DAY = 1000 * 60 * 60 * 24;
 const MS_WEEK = MS_DAY*7;
 
+/**
+* Week times
+* @typedef {Object} WeekTimes
+* @param {Number} start - Unix time for start of the week
+* @param {Number} end - Unix time for end of the week
+*/
+
+/**
+* Get the unix time for start and end of week
+* @param {Number} [offset] - The offset from current week (e.g. -1 last week, 1 next week)
+* @returns {WeekTimes} The unix time for start and end of specified week
+*/
 exports.getWeekTimes = function(offset) {
   let start = new Date();
   start.setUTCMilliseconds(0);
@@ -13,8 +25,15 @@ exports.getWeekTimes = function(offset) {
   return { start, end };
 }
 
-exports.asyncWrap = function(asyncFunction) {
+/**
+* Wrap an async function, and do something if it errors
+* @param {Function} asyncFunction - The asyncronous function to wrap
+* @param {Function} [onError=console.error] - The function to run in event of error
+* @returns {Function}
+*/
+exports.asyncWrap = function(asyncFunction, onError) {
+  const error = onError ? onError : console.error;
   return function() {
-    asyncFunction.apply(this, arguments).then(null, console.error);
+    asyncFunction.apply(this, arguments).then(null, error);
   }
 }
