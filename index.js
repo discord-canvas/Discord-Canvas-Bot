70493bb510b7a9fca2342fe9be8d67f540c9fa28
Cammy Fentreux <cameron1709@live.co.uk>
2020-11-11 08:07:24 +0000

    comments

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
+// ----------- Set-up -----------
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs')
 require('dotenv').config()
 
 const homework = JSON.parse(fs.readFileSync('homework.json'))
+client.login(process.env.BOT_TOKEN);
+
+// ----------- Event Bindings -----------
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
+  // prep
   prefix = '!'
   if (!msg.content.startsWith(prefix) || msg.author.bot) {return}
 
   const args = msg.content.slice(prefix.length).trim().split(' ')
   const command = args.shift().toLowerCase()
 
+  // check command
   if (command === 'homework' || command === 'hw') {
     try {
+      // traverse homework tree to find requested node
       curr = homework
       path = 'questions/' + args.join('.')
 
@@ -39,14 +47,17 @@ client.on('message', msg => {
   }
 })
 
+// ----------- Helpers -----------
+
 
 function sendNode(destination, node, path) {
+  // sends all relevent information about a homework node
   if (node.image) {
     image = fs.readFileSync(path + '.png')
     destination.send("", {files:[image]})
   }
 
-  message = ''
+  message = ''  // collates messages into one rather than sending many
   if (node.alt_desc) {
     message += `\n${node.alt_desc}\n`
   }
@@ -56,11 +67,10 @@ function sendNode(destination, node, path) {
       message += `\t${key}\n`
     }
   }
+
   if (message) {destination.send(message)}
 }
 
-client.login(process.env.BOT_TOKEN);
 
 // TODO:
-// Add error catching for invalid ids
 // Add !help command
