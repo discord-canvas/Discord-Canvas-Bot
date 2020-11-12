e9adb6fb145fb39a5768b4f94ce7ea5d0d9c41c1
Cammy Fentreux <cameron1709@live.co.uk>
2020-11-12 11:13:16 +0000

    added branching arguments

--- src/commands/homework.js
+++ src/commands/homework.js
@@ -6,18 +6,42 @@ const { sendNode } = require('../homeworkutils.js');
 
 const homework = JSON.parse(fs.readFileSync('homework.json'));
 
-const call = async function(msg, args) {
+// const call = async function(msg, args) {
+//   try {
+//     // traverse homework tree to find requested node
+//     curr = homework
+//
+//     for (var i = 0; i < args.length-1; i++) {
+//       curr = curr.children[args[i].toLowerCase()]
+//     }
+//
+//     console.log(`${msg.author.tag} requested ${path}`)
+//     if (args[i].includes(",")) {  // if path is end-branching
+//       branch = args.pop()
+//       branch = branch.split(',')
+//       path = 'questions/' + args.join('.')
+//
+//       await sendNode(msg.channel, curr, path + branch[0])
+//
+//     } else {
+//       path = 'questions/' + args.join('.')
+//       await sendNode(msg.channel, curr, path)
+//     }
+//   } catch (err) {
+//     console.warn(err)
+//     switch (err.name) {
+//       case 'TypeError':
+//         await msg.channel.send(`${path} is not a question I know about`)
+//         break
+//       default:
+//         await msg.channel.send('Unknown error detected, aborting operation.')
+//     }
+//   }
+// }
+
+const call = async function (msg, args) {
   try {
-    // traverse homework tree to find requested node
-    curr = homework
-    path = 'questions/' + args.join('.')
-
-    for (let i = 0; i < args.length; i++) {
-      curr = curr.children[args[i].toLowerCase()]
-    }
-
-    console.log(`${msg.author.tag} requested ${path}`)
-    await sendNode(msg.channel, curr, path)
+    travelTree(homework, args, 'questions/', msg.channel)
   } catch (err) {
     console.warn(err)
     switch (err.name) {
@@ -28,6 +52,22 @@ const call = async function(msg, args) {
         await msg.channel.send('Unknown error detected, aborting operation.')
     }
   }
+}
+
+function travelTree(curr, args, path, destination) {
+  // check for and handle branching arg
+  if (args[0].includes(',')) {
+    [args[0], branch] = args.shift().split(',')
+    sendNode(destination, curr.children[branch], `${path}${branch}`)
+  }
+
+  // base case
+  if (args.length == 1) {
+    sendNode(destination, curr.children[args[0]], `${path}${args[0]}`)
+    return
+  }
+
+  travelTree(curr.children[args[0]], args.slice(1),`${path}${args[0]}.`, destination)
 }
 
 exports.name = ['homework','hw'];
