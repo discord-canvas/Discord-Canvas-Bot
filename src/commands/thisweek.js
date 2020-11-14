daa3c86ebdd656527313f8fa807741b4436b99a0
Douile <25043847+Douile@users.noreply.github.com>
2020-11-14 22:50:42 +0000

    Remove unecessary semicolon

--- src/commands/thisweek.js
+++ src/commands/thisweek.js
@@ -4,4 +4,4 @@ const { upcomingAssignments } = require('./templates/assignments.js');
 
 exports.name = 'thisweek';
 exports.call = upcomingAssignments();
-exports.help = 'Output assignments due this week\n`!thisweek [filter]` e.g. `!thisweek 105`';;
+exports.help = 'Output assignments due this week\n`!thisweek [filter]` e.g. `!thisweek 105`';
