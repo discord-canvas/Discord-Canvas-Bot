56fd5a6fed75d2f400646508c6a7391c66d845d6
Douile <25043847+Douile@users.noreply.github.com>
2020-11-17 19:45:15 +0000

    Add week aliases
    nw - nextweek
    tw - thisweek

--- src/commands/nextweek.js
+++ src/commands/nextweek.js
@@ -2,6 +2,6 @@
 
 const { upcomingAssignments } = require('./templates/assignments.js');
 
-exports.name = 'nextweek';
+exports.name = ['nextweek', 'nw];
 exports.call = upcomingAssignments(1);
 exports.help = 'Output assignments due next week\n`!nextweek [filter]` e.g. `!nextweek 105`';
--- src/commands/thisweek.js
+++ src/commands/thisweek.js
@@ -2,6 +2,6 @@
 
 const { upcomingAssignments } = require('./templates/assignments.js');
 
-exports.name = 'thisweek';
+exports.name = ['thisweek', 'tw'];
 exports.call = upcomingAssignments();
 exports.help = 'Output assignments due this week\n`!thisweek [filter]` e.g. `!thisweek 105`';
