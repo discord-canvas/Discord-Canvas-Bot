f0be6a66eb0d4f0e7c02c1da2364bc62ff6aa73b
Douile <25043847+Douile@users.noreply.github.com>
2021-10-07 23:45:54 +0100

    Add command ittefnoc

---
+++ src/commands/ittefnoc.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { MessageEmbed, MessageAttachment } = require('discord.js-light');
+const fs = require('fs').promises;
+const {join} = require('path');
+
+const { EMBED_COLOR } = require('../constants.js')
+
+const call = async function(message) {
+  const embed = new MessageEmbed({ color: EMBED_COLOR, image: { url: 'attachment://ittefnoc.gif' } });
+  const imageBuffer = await fs.readFile(join(__dirname,'../../assets/ittefnoc.gif'));
+  embed.attachFiles(new MessageAttachment(imageBuffer, 'ittefnoc.gif'));
+  await message.channel.send({embed});
+}
+
+exports.name = 'ittefnoc';
+exports.call = call;
+exports.help = '🎉';
