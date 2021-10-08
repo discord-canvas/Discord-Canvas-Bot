6535b026314bd1f77914bc651245b51aadf22b82
Douile <25043847+Douile@users.noreply.github.com>
2021-10-08 12:27:17 +0100

    Delete 211 command

--- src/commands/211.js
+++
@@ -1,9 +0,0 @@
-const start = 1633690800;
-
-const call = async function(message) {
-  const time = Date.now() - start;
-  await message.channel.send(`We have been watching tutorial for ${Math.round(time/1000/60)} minutes...`);
-}
-
-exports.name = '211';
-exports.call = call;
